fa3c7d94a64d34bdf44ff312c8ef9e79c7bb6603
Abdullah <altaiabod43@gmail.com>
2026-07-21 15:41:35 +0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// ضع بيانات مشروع Supabase الجديد هنا فقط.
+window.IBRAQ_SUPABASE = {
+  url: "PASTE_YOUR_SUPABASE_URL_HERE",
+  publishableKey: "PASTE_YOUR_PUBLISHABLE_KEY_HERE",
+  bucket: "product-images"
+};
